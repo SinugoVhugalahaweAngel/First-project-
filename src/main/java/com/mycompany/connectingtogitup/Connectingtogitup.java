034861d0c2ef5a1d996353e56a869aac0fff357e
RC_Student_lab <RC_Student_lab@10.0.1.3>
2024-08-28 12:07:21 +0000

    First commit

---
+++ src/main/java/com/mycompany/connectingtogitup/Connectingtogitup.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.connectingtogitup;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Connectingtogitup {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
